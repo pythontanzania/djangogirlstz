260a6bbb1f003c625642bbd5fc66ef068530cd65
Your Name <you@example.com>
2024-10-08 10:14:23 +0300

    updates on subscription for newsletter

--- djangogirlstz/views.py
+++ djangogirlstz/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, redirect
-
 from django.views.generic import TemplateView
 from .forms import SubscriptionForm
 
 class HomePageView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = SubscriptionForm()  # Add subscription form to the context
+        return context
+
 def community(request):
-    return render(request, 'community.html')
+    form = SubscriptionForm()
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'community.html', {'form': form})
 
 def resources(request):
-    return render(request, 'resources.html')
+    form = SubscriptionForm()
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'resources.html', {'form': form})
 
 def events(request):
-    return render(request, 'events.html')
+    form = SubscriptionForm()
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'events.html', {'form': form})
 
 def dar(request):
-    return render(request, 'dar.html')
+    form = SubscriptionForm()  
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'dar.html', {'form': form})
 
 def register(request):
     return render(request, 'register.html')
@@ -25,11 +49,9 @@ def login_view(request):
     return render(request, 'login.html')
 
 def subscribe(request):
-    if request.method == "POST":
-        form = SubscriptionForm(request.POST)
+    form = SubscriptionForm()
+    if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('success')  # Redirect to a success page or another page
-    else:
-        form = SubscriptionForm()
-    return render(request, 'subscribe.html', {'form': form})
+    return redirect('home')  
+
