87d4a3fcc5d78345a2bb7199cc132bd75e057f94
Your Name <you@example.com>
2024-10-10 16:04:21 +0300

    Updated hero section for improved responsiveness, padding/margin adjustments, and image alignment

--- djangogirlstz/models.py
+++ djangogirlstz/models.py
@@ -7,3 +7,18 @@ class Subscription(models.Model):
 
     def __str__(self):
         return self.email
+    
+class User(models.Model):
+    school_email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=128)  # Use a hashed password in practice
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+    
+
+
+
+    
--- djangogirlstz/views.py
+++ djangogirlstz/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import TemplateView
-from .forms import SubscriptionForm
+from .forms import SubscriptionForm 
+from .forms import UserRegistrationForm
+from .forms import LoginForm
 
 class HomePageView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = SubscriptionForm()  # Add subscription form to the context
+        context['form'] = SubscriptionForm()  
         return context
 
 def community(request):
@@ -43,10 +46,33 @@ def dar(request):
     return render(request, 'dar.html', {'form': form})
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            # Use email for username if you have set up User model accordingly
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to the home page or dashboard
+            else:
+                form.add_error(None, "Invalid email or password.")
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login.html', {'form': form})
 
 def subscribe(request):
     form = SubscriptionForm()
